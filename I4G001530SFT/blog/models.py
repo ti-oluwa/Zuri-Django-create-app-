e25ecec6761c01392dd66dfbd3c9de1f1b62e28e
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2022-06-15 00:01:04 +0100

    new commit
    Zuri task

---
+++ I4G001530SFT/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.contrib.auth import get_user_model
+
+
+current_user = get_user_model()
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(current_user, default=None, on_delete=models.CASCADE)
+    created_date = models.DateTimeField("Date created")
+    published_date = models.DateTimeField("Date published")
+ 
+    def __str__(self):
+        return self.title
+    
